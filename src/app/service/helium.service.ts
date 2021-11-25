586a0117330230859127c8cf832c6a722d3cd5bb
Merlijn <bergvandenmerlijn@gmail.com>
2021-11-25 17:00:51 +0100

    improved api call

--- src/app/service/helium.service.ts
+++ src/app/service/helium.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import {catchError, map} from "rxjs/operators";
+import {Observable, throwError} from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +17,7 @@ export class HeliumService {
   private accountAdres = '14drLjW9EfZ2E5zXwURaaZHnXxWcimmLPCjZdLSfzcxz7NXqkiL';
   
   getData(){
-    return this.http.get(this.url + 'accounts/14drLjW9EfZ2E5zXwURaaZHnXxWcimmLPCjZdLSfzcxz7NXqkiL');
+	return this.http.get(this.url + 'accounts/14drLjW9EfZ2E5zXwURaaZHnXxWcimmLPCjZdLSfzcxz7NXqkiL');
   }
 
 
@@ -25,20 +28,20 @@ export class HeliumService {
 
   //general account info
   getAccountAddres(){
-    let params = 'accounts/' + this.accountAdres;
-    return this.http.get(this.url + params);
+	let params = 'accounts/' + this.accountAdres;
+	return this.http.get(this.url + params);
   }
 
   //get all hotspots by account
   getHotspotsForAccount(){
-    let params = 'accounts/' + this.accountAdres + '/hotspots';
-    return this.http.get(this.url + params);
+	let params = 'accounts/' + this.accountAdres + '/hotspots';
+	return this.http.get(this.url + params);
   }
 
   //get rewards for account
   getRewardsForAccount(){
-    let params = 'accounts/' + this.accountAdres + '/rewards';
-    return this.http.get(this.url + params);
+	let params = 'accounts/' + this.accountAdres + '/rewards';
+	return this.http.get(this.url + params);
   }
 
 
@@ -50,15 +53,15 @@ export class HeliumService {
   * or in relative time (e.g. -1 week, which when url esacped becomes -1%20week). When ommitted the current time is assumed.
   */
   getRewardTotalForAccount(){
-    let sum = '?min_time=-7%20day&bucket=day';
-    let params = 'accounts/' + this.accountAdres + '/rewards/' + sum;
-    let response = this.http.get(this.url + params);
-    
-    //TODO use the cursor to get the real data
-    
-    // let data = this.http.get(this.url + '/blocks?cursor=' + c);
+	let sum = '?min_time=-7%20day&bucket=day';
+	let params = 'accounts/' + this.accountAdres + '/rewards/' + sum;
+	let response = this.http.get(this.url + params);
+	
+	//TODO use the cursor to get the real data
+	
+	// let data = this.http.get(this.url + '/blocks?cursor=' + c);
 
-    return response;  //todo return data
+	return response;  //todo return data
   }
  
  
@@ -67,13 +70,27 @@ export class HeliumService {
   ////////////////////////////////////////////////////////////////
   
   /*
+    TODO: make sure in app.component.ts we dont need subscribe anymore
   * get hotspot by name
   * The name must be all lower-case with dashes between the words
   * example: 'tangy-blood-lion'
   */
-  getHotspotByName(name:string){
-    let params = 'hotspots/' + 'name' + name;
-    return this.http.get(this.url + params);
+	getHotspotByName(name:string){
+		let params = 'hotspots/' + 'name/' + name;
+		//return this.http.get(this.url + params);
+		return this.http
+      .get(
+        this.url + params, {responseType: 'json'}
+      ).pipe(
+        map(responseData => {
+          console.log('API response', responseData)
+          return responseData;
+      }),
+      catchError(errorRes => {
+          console.log('API response', errorRes)
+          return throwError(errorRes);
+      })
+    );
   }
   
    /*
@@ -83,8 +100,8 @@ export class HeliumService {
   * here we can also add params for min and max time
   */
    getTotalRewardsByHotspot(name:string){
-    let params = 'hotspots/' + 'name' + name;
-    return this.http.get(this.url + params);
+	let params = 'hotspots/' + 'name' + name;
+	return this.http.get(this.url + params);
   }
 
 }
