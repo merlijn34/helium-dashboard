9749df294a8fd1ae4b65d932d6be8b3ea4a80b0c
Merlijn <bergvandenmerlijn@gmail.com>
2021-11-29 22:44:12 +0100

    Revert "Angular Material sideNav implementaiton"
    This reverts commit bd49a2aec0a76c3218a4d15c620f14a112844b90.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MyHotspotsComponent } from './components/my-hotspots/my-hotspots.component';
-import { DashboardModule } from './components/dashboard.module';
 
-const routes: Routes = [
-	{
-		path: '',
-		loadChildren: () => import('./components/dashboard.module').then(m => m.DashboardModule)
-	},
-	{
-		path: '**',
-		redirectTo: '',
-		pathMatch: 'full'
-	}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/nav/nav.component.spec.ts
+++ src/app/components/nav/nav.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WrapperComponent } from './wrapper.component';
+import { NavComponent } from './nav.component';
 
-describe('WrapperComponent', () => {
-  let component: WrapperComponent;
-  let fixture: ComponentFixture<WrapperComponent>;
+describe('NavComponent', () => {
+  let component: NavComponent;
+  let fixture: ComponentFixture<NavComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WrapperComponent ]
+      declarations: [ NavComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WrapperComponent);
+    fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
